997af0eada156790539b824e4bf3c95208154a68
Vasileios Loumanis <loumanv@gmail.com>
2018-06-11 10:18:24 +0100

    Creates StackOverflowUsers project Cleanup and organisation of files Adds SwiftLint

---
+++ StackOverflowUsers/App/AppDelegate/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  StackOverflowUsers
+//
+//  Created by Vasileios Loumanis on 11/06/2018.
+//  Copyright © 2018 Vasileios Loumanis. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions
+        launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
